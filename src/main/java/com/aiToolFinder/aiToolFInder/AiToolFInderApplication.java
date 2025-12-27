cf2004dd4b3685cb0ecfcb8a1b0efead39f59782
Dhinesh Kumar Selvam(UST <304505@ust.com>
2025-12-27 11:29:43 +0530

    Maven Structure

---
+++ src/main/java/com/aiToolFinder/aiToolFInder/AiToolFInderApplication.java
@@ -0,0 +1,13 @@
+package com.aiToolFinder.aiToolFInder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiToolFInderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiToolFInderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiToolFinder/aiToolFInder/AiToolFInderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiToolFinder.aiToolFInder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiToolFInderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
